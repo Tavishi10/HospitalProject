a2806eca637cce89de87457633d7cb74e777041b
Tavishi Suvarna <suvarnatavishi7@gmail.com>
2023-01-09 00:04:24 +0530

    added views and routes for doctors nurses and staff

---
+++ hospitalapp/nurse.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from .models import Department, Nurse
+import json
+from datetime import datetime
+
+@csrf_exempt
+def get_all_nurse(request):
+    if (request.method == "GET"):
+        #Serialize the data into json
+        data = serializers.serialize("json", Nurse.objects.all())
+        # Turn the JSON data into a dict and send as JSON response
+        return JsonResponse(json.loads(data), safe=False)
+
+@csrf_exempt
+def get_nurse_by_id(request, id):
+    if(request.method == "GET"):
+        data = serializers.serialize("json", Nurse.objects.filter(pk=id))
+        return JsonResponse(json.loads(data), safe=False)
+
+@csrf_exempt
+def create(request):
+    if (request.method == "POST"):
+        body = json.loads(request.body.decode("utf-8"))
+        dept = Department.objects.get(pk = body['dept_id'])
+        date = datetime.strptime(body['date_of_birth'], "%Y-%m-%d").date()
+        newrecord = Nurse.objects.create(name=body['name'], contact_no=body['contact_no'], email_id=body['email_id'], date_of_birth = date, specialization = body['specialization'], salary=body['salary'], dept_id = dept)
+        data = json.loads(serializers.serialize('json', [newrecord]))
+        return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def delete(request, id):
+    if (request.method == "DELETE"):
+        Nurse.objects.filter(pk=id).delete()
+        newrecord = Nurse.objects.all()
+        data = json.loads(serializers.serialize('json', newrecord))
+        return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def edit(request, id):
+    if (request.method == "PUT"):
+        pass
--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views
-from . import department
+from . import department, nurse, staff, doctor
 
 urlpatterns = [
     path('department/', department.get_all, name='get_all'),
@@ -8,4 +7,19 @@ urlpatterns = [
     path('department/get/<int:id>', department.get_by_id, name='get_by_id'),
     path('department/edit/<int:id>', department.edit, name='edit'),
     path('department/delete/<int:id>', department.delete, name='delete'),
+    path('staff/', staff.get_all_staff, name='get_all_staff'),
+    path('staff/create/', staff.create, name='create'),
+    path('staff/get/<int:id>', staff.get_staff_by_id, name='get_staff_by_id'),
+    path('staff/edit/<int:id>', staff.edit, name='edit'),
+    path('staff/delete/<int:id>', staff.delete, name='delete'),
+    path('nurse/', nurse.get_all_nurse, name='get_all_nurse'),
+    path('nurse/create/', nurse.create, name='create'),
+    path('nurse/get/<int:id>', nurse.get_nurse_by_id, name='get_nurse_by_id'),
+    path('nurse/edit/<int:id>', nurse.edit, name='edit'),
+    path('nurse/delete/<int:id>', nurse.delete, name='delete'),
+    path('doctor/', doctor.get_all_doctor, name='get_all_doctor'),
+    path('doctor/create/', doctor.create, name='create'),
+    path('doctor/get/<int:id>', doctor.get_doctor_by_id, name='get_doctor_by_id'),
+    path('doctor/edit/<int:id>', doctor.edit, name='edit'),
+    path('doctor/delete/<int:id>', doctor.delete, name='delete'),
 ]
