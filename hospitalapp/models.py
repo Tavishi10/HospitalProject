183b4b33b353ab06480924fb034f2e6752e80e27
Tavishi Suvarna <suvarnatavishi7@gmail.com>
2023-01-09 22:57:11 +0530

    created migrations

--- hospitalapp/models.py
+++ hospitalapp/models.py
@@ -38,3 +38,32 @@ class Nurse(Person):
 
     def __str__(self):
         return '%s %s %s' %(self.name, self.contact_no, self.specialization)
+
+class OpdPatient(Person):
+    address = models.CharField(max_length=60)
+    gender = models.CharField(max_length=10)
+    bill_amount = models.DecimalField(decimal_places=2, max_digits=15)
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return '%s %s %s %s' %(self.name, self.contact_no, self.address, self.gender)
+
+class IpdPatient(Person):
+    address = models.CharField(max_length=60)
+    gender = models.CharField(max_length=10)
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
+    nurses = models.ManyToManyField(Nurse)
+    ward = models.CharField(max_length=20)
+    medicine_charges = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    bloodcheck_charges = models.DecimalField(decimal_places=2, default= 0, max_digits=15)
+    radiology_charges = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    injection_charges = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    laundary_charges = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    misc_charges = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    bill_amount = models.DecimalField(decimal_places=2, default=0, max_digits=15)
+    bill_paid = models.BooleanField(default=False)
+    admission_date = models.DateField(auto_now_add=True)
+    discharge_date = models.DateField()
+
+    def __str__(self):
+        return '%s %s %s %s' %(self.name, self.contact_no, self.address, self.gender, self.ward)
